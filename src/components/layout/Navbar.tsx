9aef955af9f28917b97e81b4e3030c5f7395d54d
Nalin Dalal <nalindalal2004@gmail.com>
2026-04-18 17:28:32 +0530

    Add logo.svg to navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -36,8 +36,8 @@ export function Navbar() {
     >
       <div className="max-w-[1200px] mx-auto px-6 lg:px-12">
         <div className="flex items-center justify-between h-16">
-          <Link href="/" className="font-mono text-xl font-bold tracking-tight">
-            nerdev<span style={{ color: 'var(--orange)' }}>.</span>
+          <Link href="/" className="flex items-center">
+            <img src="/logo.svg" alt="nerdev" className="h-8 w-auto" />
           </Link>
           
           <div className="hidden lg:flex items-center gap-8">
